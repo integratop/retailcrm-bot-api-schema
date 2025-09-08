ace9462cfa30c682ef3360e220ee5c53dfc0566f
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2025-09-08 19:21:02 +0300

    fix: упрощение и причёсывание скриптов

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true,
+    passWithNoTests: true,
     environment: "node",
     sequence: { hooks: "list" },
     testTimeout: 60_000,
-    clearMocks: true,
     // setupFiles: ["dotenv/config"],
+    clearMocks: true,
     include: ["**/*.test.ts"],
     exclude: ["node_modules"],
   },
